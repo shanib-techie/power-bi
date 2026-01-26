c665c38e8f6c4b445741e2a05779f67ccb4c3442
shanib-techie <shanibkhan622@gmail.com>
2026-01-26 13:44:28 +0530

    projectofrentedhouse

---
+++ house_rented/irish_rent.py
@@ -0,0 +1,5 @@
+import pandas as pd
+df = pd.read_csv("C:\\Users\\Admin\\OneDrive\\Desktop\\power bi\\house_rented\\irish_rent_by_county.csv")
+df.to_excel("C:\\Users\\Admin\\OneDrive\\Desktop\\power bi\\house_rented\\irish_rent_by_county.xlsx")
+print(df)
+# "C:\Users\Admin\OneDrive\Desktop\power bi\deep_insight\employees.xlsx"
