3973683c68988b263fb50df32d8dd6498284de4c
shanib-techie <shanibkhan622@gmail.com>
2026-02-22 22:43:01 +0530

    ad

--- employees_dat(not_useful)/empl.py
+++ employees_dat(not_useful)/empl.py
@@ -1,5 +1,5 @@
 import pandas as pd
 df = pd.read_json("C:\\Users\\Admin\\OneDrive\\Desktop\\power bi\\deep_insight\\employees.json")
-df.to_excel("C:\\Users\\Admin\\OneDrive\\Desktop\\power bi\\deep_insight\\employees.xlsx",index=False)
+df.to_html("C:\\Users\\Admin\\OneDrive\\Desktop\\power bi\\deep_insight\\employees.html",index=False)
 print(df)
 
