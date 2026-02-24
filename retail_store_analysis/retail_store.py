e79a7a45dfb378f3063db5bc7bd61e82f07bd209
shanib-techie <shanibkhan622@gmail.com>
2026-02-24 11:54:00 +0530

    add

--- retail_store_analysis/retail_store.py
+++ retail_store_analysis/retail_store.py
@@ -19,14 +19,76 @@ print("how much time store manager sales",df["StoreManager"].value_counts())
 print(df["StoreManager"].unique())
 print("cashier",df["Cashier"].unique())
 print(df["Cashier"].value_counts())
-print(max(df["TotalPrice"]))
+# print(max(df["TotalPrice"]))
 
 
-print("WHERE LOCATION IS store C")
+
+
+
+
+print("WHERE LOCATION IS store C and product is tablet")
 print("count of the following ",len(df[(df["Location"] == "Store C")  & (df["Product"] == "Tablet")]))
 print(df[(df["Location"] == "Store C")  & (df["Product"] == "Tablet")])
 
+
+r_from_c = df[df["Location"] == "Store C"]["TotalPrice"].sum()
+print("total revenue from c store",r_from_c)
+
+
+r_from_d = df[df["Location"] == "Store D"]["TotalPrice"].sum()
+print("revenue from d location",r_from_d)
+
+
+print("revenue from each location ")
+r_each_l = df.groupby("Location")["TotalPrice"].sum()
+print(r_each_l)
+
+
+
+
+
+
+
+tablet_revenue = df[df["Product"] == "Tablet"]["TotalPrice"].sum()
+print("How much revenue from Tablet:", tablet_revenue)
+printer_revenue = df[df["Product"] == "Printer"]["TotalPrice"].sum()
+print("how much revenue from printer",printer_revenue)
+phone_revenue = df[df["Product"] == "Phone"]["TotalPrice"].sum()
+print("how much revenue from phone",phone_revenue)
+monitor_revenue = df[df["Product"] == "Monitor"]["TotalPrice"].sum()
+print("revenue from monitor ",monitor_revenue)
+laptop_revenue = df[df["Product"] == "Laptop"]["TotalPrice"].sum()
+print("revenue from Laptop",laptop_revenue)
+chair_revenue = df[df["Product"] == "Chair"]["TotalPrice"].sum()
+print("revenue from chair ",chair_revenue)
+desk_revenue = df[df["Product"] == "Desk"]["TotalPrice"].sum()
+print("REvenue from desk",desk_revenue)
+
+
+
+r_each_p = df.groupby("Product")["TotalPrice"].sum()
+print("revenue of each product",r_each_p)
+
+
+
+
+
+
 print(df)
+revenue = df.groupby(["Location", "Product"])["TotalPrice"].sum()#kaam ka function h
+
+print("revenue from ech location with every product",revenue)
+
+
+# ANYLSIS ON  PaymentType
+print("PAYMENT METHOD ==>",df["PaymentType"].unique())
+print("count of payment method ")
+print(df["PaymentType"].value_counts())
+# kis loacation se kitne kitne payment type hue h 
+loc_with_payment_type = df.groupby(["Location","PaymentType"])["TotalPrice"].sum()
+print("each location with count of payment method ",loc_with_payment_type)
+loc_with_payment_type_count = df.groupby(["Location","PaymentType"]).size()
+print(loc_with_payment_type_count)
 
 # print(df)
 df.to_html("C:\\Users\\Admin\\OneDrive\\Desktop\\power bi\\retail_store_analysis\\Retail-Store-Transactions (1).html")
