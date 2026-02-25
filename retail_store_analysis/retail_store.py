ef4a0cd9da5454b7a243728b6bc51cb0368031ad
shanib-techie <shanibkhan622@gmail.com>
2026-02-25 15:33:08 +0530

    plt

--- retail_store_analysis/retail_store.py
+++ retail_store_analysis/retail_store.py
@@ -90,15 +90,77 @@ print("each location with count of payment method ",loc_with_payment_type)
 loc_with_payment_type_count = df.groupby(["Location","PaymentType"]).size()
 print(loc_with_payment_type_count)
 
-loc_with_payment_type_count_make_graph = df.groupby(["Location","PaymentType"]).size().unstack()
+# loc_with_payment_type_count_make_graph = df.groupby(["Location","PaymentType"]).size().unstack()
 
-loc_with_payment_type_count_make_graph.plot(kind="bar")
+# loc_with_payment_type_count_make_graph.plot(kind="bar")
 
-plt.xlabel("Stores")
-plt.ylabel("Count of Payment Type")
-plt.title("Payment Type Count per Store")
+# # plt.xlabel("Stores")
+# plt.ylabel("Count of Payment Type")
+# plt.title("Payment Type Count per Store")
 
-plt.legend(title="Payment Type")
-plt.show()
+# plt.legend(title="Payment Type")
+# plt.show()
 # print(df)
 df.to_html("C:\\Users\\Admin\\OneDrive\\Desktop\\power bi\\retail_store_analysis\\Retail-Store-Transactions (1).html")
+
+# operation on store manager
+
+storemanager_names = df["StoreManager"].unique()
+print("NUMBER OF STORE MANAGER = > ",len(storemanager_names))
+print("store manager = >",storemanager_names)
+
+# each store manager total sale 
+print("each store manager total sale :")
+val_of_each_store_manager = df["StoreManager"].value_counts()
+print(val_of_each_store_manager)
+
+print("DID CASHIER AND STORRE MANAGER ARE SAME ??")
+grp_of_cashier_and_manager = df.groupby(["Cashier","StoreManager"]).size()
+print(grp_of_cashier_and_manager)
+
+# grp_of_cashier_and_manager_graph = df.groupby(["Cashier","StoreManager"]).size().unstack()
+
+# grp_of_cashier_and_manager_graph.plot(kind="bar")
+# plt.title("cashier group with manager")
+# plt.grid("-")
+# plt.ylabel("count of cashier with differnet store manager ")
+# plt.show()
+
+
+
+
+
+grp_of_manager_with_product = df.groupby(["StoreManager" , "Product"])["TotalPrice"].sum()
+print("STORE MANAGER WITH HIS SALES IN EACH PRODUCT : ")
+print(grp_of_manager_with_product)
+
+
+
+print(df["Cashier"].unique())
+# ///////////////////////////////GROUP GRAPH OF EACH STORE MANAGER PERTICULAR ITEM SALES
+# grp_of_manager_with_product_grap = df.groupby(["StoreManager" , "Product"])["TotalPrice"].sum().unstack()
+
+# grp_of_manager_with_product_grap.plot(kind = "bar")
+# plt.grid()
+# plt.xlabel("store manager ")
+# plt.show()
+
+
+
+
+
+# ////////////////////////////////////////bar graph of each storemanager sales count
+# plt.bar(val_of_each_store_manager.index,val_of_each_store_manager.values,color="yellow",label="store manager sales")
+# plt.xlabel("store manager")
+# plt.ylabel("store manager count of sales")
+# plt.show()
+
+
+
+# pie chart of store manager with its sale
+sale_of_each_storemaneger = df.groupby(["StoreManager"])["TotalPrice"].sum()
+print(sale_of_each_storemaneger)
+plt.pie(sale_of_each_storemaneger,labels=sale_of_each_storemaneger.index,autopct="%1.1f%%",colors=["red","yellow","pink","green","blue"])#label  pie ki per seace ki valy
+plt.grid()
+plt.title("SALES OF EACH STORE MANAGER ")
+plt.show()
