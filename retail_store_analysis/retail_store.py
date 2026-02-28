2070410f684e84db660d83337dff9736865fea39
shanib-techie <shanibkhan622@gmail.com>
2026-02-28 15:50:11 +0530

    ad

--- retail_store_analysis/retail_store.py
+++ retail_store_analysis/retail_store.py
@@ -31,38 +31,43 @@ print("count of the following ",len(df[(df["Location"] == "Store C")  & (df["Pro
 print(df[(df["Location"] == "Store C")  & (df["Product"] == "Tablet")])
 
 
-r_from_c = df[df["Location"] == "Store C"]["TotalPrice"].sum()
-print("total revenue from c store",r_from_c)
+# r_from_c = df[df["Location"] == "Store C"]["TotalPrice"].sum()
+# print("total revenue from c store",r_from_c)
 
 
-r_from_d = df[df["Location"] == "Store D"]["TotalPrice"].sum()
-print("revenue from d location",r_from_d)
+# r_from_d = df[df["Location"] == "Store D"]["TotalPrice"].sum()
+# print("revenue from d location",r_from_d)
 
 
 print("revenue from each location ")
 r_each_l = df.groupby("Location")["TotalPrice"].sum()
 print(r_each_l)
 
+r_each_l_graph = df.groupby("Location")["TotalPrice"].sum()
+r_each_l_graph.plot(kind="bar")
+# plt.bar(r_each_l_graph)
 
 
 
 
 
 
-tablet_revenue = df[df["Product"] == "Tablet"]["TotalPrice"].sum()
-print("How much revenue from Tablet:", tablet_revenue)
-printer_revenue = df[df["Product"] == "Printer"]["TotalPrice"].sum()
-print("how much revenue from printer",printer_revenue)
-phone_revenue = df[df["Product"] == "Phone"]["TotalPrice"].sum()
-print("how much revenue from phone",phone_revenue)
-monitor_revenue = df[df["Product"] == "Monitor"]["TotalPrice"].sum()
-print("revenue from monitor ",monitor_revenue)
-laptop_revenue = df[df["Product"] == "Laptop"]["TotalPrice"].sum()
-print("revenue from Laptop",laptop_revenue)
-chair_revenue = df[df["Product"] == "Chair"]["TotalPrice"].sum()
-print("revenue from chair ",chair_revenue)
-desk_revenue = df[df["Product"] == "Desk"]["TotalPrice"].sum()
-print("REvenue from desk",desk_revenue)
+
+
+# tablet_revenue = df[df["Product"] == "Tablet"]["TotalPrice"].sum()
+# print("How much revenue from Tablet:", tablet_revenue)
+# printer_revenue = df[df["Product"] == "Printer"]["TotalPrice"].sum()
+# print("how much revenue from printer",printer_revenue)
+# phone_revenue = df[df["Product"] == "Phone"]["TotalPrice"].sum()
+# print("how much revenue from phone",phone_revenue)
+# monitor_revenue = df[df["Product"] == "Monitor"]["TotalPrice"].sum()
+# print("revenue from monitor ",monitor_revenue)
+# laptop_revenue = df[df["Product"] == "Laptop"]["TotalPrice"].sum()
+# print("revenue from Laptop",laptop_revenue)
+# chair_revenue = df[df["Product"] == "Chair"]["TotalPrice"].sum()
+# print("revenue from chair ",chair_revenue)
+# desk_revenue = df[df["Product"] == "Desk"]["TotalPrice"].sum()
+# print("REvenue from desk",desk_revenue)
 
 
 
@@ -70,14 +75,11 @@ r_each_p = df.groupby("Product")["TotalPrice"].sum()
 print("revenue of each product",r_each_p)
 
 
-
-
-
-
 print(df)
 revenue = df.groupby(["Location", "Product"])["TotalPrice"].sum()#kaam ka function h
 
 print("revenue from ech location with every product",revenue)
+
 
 
 # ANYLSIS ON  PaymentType
@@ -89,6 +91,10 @@ loc_with_payment_type = df.groupby(["Location","PaymentType"])["TotalPrice"].sum
 print("each location with count of payment method ",loc_with_payment_type)
 loc_with_payment_type_count = df.groupby(["Location","PaymentType"]).size()
 print(loc_with_payment_type_count)
+
+
+
+plt.show()
 
 # loc_with_payment_type_count_make_graph = df.groupby(["Location","PaymentType"]).size().unstack()
 
@@ -105,18 +111,18 @@ df.to_html("C:\\Users\\Admin\\OneDrive\\Desktop\\power bi\\retail_store_analysis
 
 # operation on store manager
 
-storemanager_names = df["StoreManager"].unique()
-print("NUMBER OF STORE MANAGER = > ",len(storemanager_names))
-print("store manager = >",storemanager_names)
+# storemanager_names = df["StoreManager"].unique()
+# print("NUMBER OF STORE MANAGER = > ",len(storemanager_names))
+# print("store manager = >",storemanager_names)
 
-# each store manager total sale 
-print("each store manager total sale :")
-val_of_each_store_manager = df["StoreManager"].value_counts()
-print(val_of_each_store_manager)
+# # each store manager total sale 
+# print("each store manager total sale :")
+# val_of_each_store_manager = df["StoreManager"].value_counts()
+# print(val_of_each_store_manager)
 
-print("DID CASHIER AND STORRE MANAGER ARE SAME ??")
-grp_of_cashier_and_manager = df.groupby(["Cashier","StoreManager"]).size()
-print(grp_of_cashier_and_manager)
+# print("DID CASHIER AND STORRE MANAGER ARE SAME ??")
+# grp_of_cashier_and_manager = df.groupby(["Cashier","StoreManager"]).size()
+# print(grp_of_cashier_and_manager)
 
 # grp_of_cashier_and_manager_graph = df.groupby(["Cashier","StoreManager"]).size().unstack()
 
@@ -130,13 +136,13 @@ print(grp_of_cashier_and_manager)
 
 
 
-grp_of_manager_with_product = df.groupby(["StoreManager" , "Product"])["TotalPrice"].sum()
-print("STORE MANAGER WITH HIS SALES IN EACH PRODUCT : ")
-print(grp_of_manager_with_product)
+# grp_of_manager_with_product = df.groupby(["StoreManager" , "Product"])["TotalPrice"].sum()
+# print("STORE MANAGER WITH HIS SALES IN EACH PRODUCT : ")
+# print(grp_of_manager_with_product)
 
 
 
-print(df["Cashier"].unique())
+# print(df["Cashier"].unique())
 # ///////////////////////////////GROUP GRAPH OF EACH STORE MANAGER PERTICULAR ITEM SALES
 # grp_of_manager_with_product_grap = df.groupby(["StoreManager" , "Product"])["TotalPrice"].sum().unstack()
 
@@ -157,10 +163,10 @@ print(df["Cashier"].unique())
 
 
 
-# pie chart of store manager with its sale
-sale_of_each_storemaneger = df.groupby(["StoreManager"])["TotalPrice"].sum()
-print(sale_of_each_storemaneger)
-plt.pie(sale_of_each_storemaneger,labels=sale_of_each_storemaneger.index,autopct="%1.1f%%",colors=["red","yellow","pink","green","blue"])#label  pie ki per seace ki valy
-plt.grid()
-plt.title("SALES OF EACH STORE MANAGER ")
-plt.show()
+# # pie chart of store manager with its sale
+# sale_of_each_storemaneger = df.groupby(["StoreManager"])["TotalPrice"].sum()
+# print(sale_of_each_storemaneger)
+# plt.pie(sale_of_each_storemaneger,labels=sale_of_each_storemaneger.index,autopct="%1.1f%%",colors=["red","yellow","pink","green","blue"])#label  pie ki per seace ki valy
+# plt.grid()
+# plt.title("SALES OF EACH STORE MANAGER ")
+# plt.show()
