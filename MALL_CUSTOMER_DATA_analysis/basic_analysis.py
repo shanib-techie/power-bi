a76a58e8c29132f1cc4e1375d7d1300eb229ce53
shanib-techie <shanibkhan622@gmail.com>
2026-02-28 18:04:17 +0530

    ad

--- MALL_CUSTOMER_DATA_analysis/basic_analysis.py
+++ MALL_CUSTOMER_DATA_analysis/basic_analysis.py
@@ -41,3 +41,17 @@ print(group_of_that_customer_who_lies_between_35_and_70)
 
 
 print("FINAL CROSS CHECK OF ALL CUSTOMER ",len(group_of_that_customer_who_lies_between_35_and_70) + len(group_where_female_score_less_35)+len(group_where_male_score_less_35)+len(group_where_female_score_more_70)+len(group_where_male_score_more_70))
+
+
+print("RICH AND VALUED CUSTOMERS")
+grp_age_bt_23_55_score_75_above_income_55_above = df[ (df["Age"] >= 23) & (df["Age"] <= 55 )  & (df["Spending Score (1-100)"] >= 75) & (df["Annual Income (k$)"] > 55) ]
+print("NUMBER OF OUR GOLDEN CUSTOMER : ",len(grp_age_bt_23_55_score_75_above_income_55_above))
+female_from_rich_cat = grp_age_bt_23_55_score_75_above_income_55_above[grp_age_bt_23_55_score_75_above_income_55_above["Gender"] == "Female"]
+print("NUMBER OF FEMALE FROM THIS CLUSTER : " ,len(female_from_rich_cat))
+male_from_rich_cat = grp_age_bt_23_55_score_75_above_income_55_above[grp_age_bt_23_55_score_75_above_income_55_above["Gender"] == "Male"]
+print("NUMBER OF MALE FROM THIS CLUSTER  :",len(male_from_rich_cat))
+
+
+print(grp_age_bt_23_55_score_75_above_income_55_above["Gender"].value_counts())
+
+print(grp_age_bt_23_55_score_75_above_income_55_above)
